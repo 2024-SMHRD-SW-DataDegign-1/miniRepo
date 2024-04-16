068b9dda54a8a576b16f77d297d4d5b4a82b8348
smhrd <smhrd@DESKTOP-A817AVJ>
2024-04-16 16:57:44 +0900

    첫번째 수정

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -1,7 +1,7 @@
 public class Ex01 {
 	public static void main(String[] args) {
 		
-		System.out.println("팀장 첫번째 커밋!");
+		System.out.println("팀장 첫번째 커밋! 수정");
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀원2 세번째 커밋!");
 		System.out.println("팀원3 네번째 커밋!");

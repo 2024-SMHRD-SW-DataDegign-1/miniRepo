c74850c797ec869d8ae46f7c9a6567249688bc08
smhrd <smhrd@DESKTOP-SNDKBV1>
2024-04-17 17:05:34 +0900

    서브이벤트 추가할게요

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -5,14 +5,7 @@ import Util.Util;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		
-		System.out.println("팀장 첫번째 커밋! 수정");
-		System.out.println("팀원1 두번째 커밋");
-		System.out.println("팀원3 네번째 커밋!");
-		System.out.println("팀원4 네번째 커밋!");
-		System.out.println("팀원5 커밋");
-		
-
+			
 		//String str = "⣿⣿⣿⣿⠿⠿⠿⢿⡿⠿⠿⠿⢿⣿⣿⣿\n⣿⣿⣿⡇ ⣤⣤⣤⡇⠀⣤⣤⣤⣿⣿⣿\n⣿⣿⣿⣇ ⠉⠉⠉⡇⠀⠉⠉⠉⣿⣿⣿\n⣿⣿⣿⠿⠿⠿⠿⠀ ⠿ ⠿⠿⠿⣿⣿\n⣿⣿⣿⣤⣤⣤⠤⠤⠤⠤⢤⣤⣤⣿⣿⣿\n⣿⣿⣿⣿⠉⠀⣤⣤⣤⣤⡀⠈⢻⣿⣿⣿\n⣿⣿⣿⣿⣄⡀⠉⠙⠛⠉⠁⣠⣾⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿";
 		//Util.print(str);
 //		SentenceGame view = new SentenceGame();

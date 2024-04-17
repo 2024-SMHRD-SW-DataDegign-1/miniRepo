4ae10d927f7f0bbdb892758f3c7a35cdbc9e647a
smhrd <smhrd@DESKTOP-H83O5V9>
2024-04-17 19:06:20 +0900

    상현 - Util 에 print메소드 추가 , 기존 메소드 print -> println으로 이름변경

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -1,5 +1,11 @@
+import java.sql.Date;
+import java.util.ArrayList;
+
+import DB.DAO;
+import DB.DTO;
 import MainGame.SentenceGame;
 import MainGame.가위바위보;
+import SubGame.Story3;
 import Util.Util;
 
 
@@ -11,8 +17,24 @@ public class Ex01 {
 //		SentenceGame view = new SentenceGame();
 //		view.Play();
 		
-		가위바위보 view = new 가위바위보();
-		view.play();
+		DAO dao = new DAO();
+
+		// 경험치와 스트레스를 수정하는 방법
+		//dao.updateUser(exp1, stress1);
+		
+		// 사용자 정보를 불러오는 방법
+		// updateUser(상승시킬exp, 상승시킬stress);
+		Story3 view = new Story3();
+		view.Play();
+		
+		ArrayList<DTO> data = dao.searchUser();
+		// 경험치
+		int exp = data.get(0).getExp();
+		int stress = data.get(0).getStress();
+		
+		System.out.println("" + exp + stress);
+
+
 	}
 }
   

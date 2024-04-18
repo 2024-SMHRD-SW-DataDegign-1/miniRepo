399d2c22963d7e5d73758456a2c656a5bbe0d3cd
smhrd <smhrd@DESKTOP-SNDKBV1>
2024-04-18 11:37:33 +0900

    클리어콘솔 추가 메인추가 - 승진

--- GitTest01/src/PlayStation/Ex01.java
+++ GitTest01/src/PlayStation/Ex01.java
@@ -5,15 +5,14 @@ import java.util.ArrayList;
 
 import DB.DAO;
 import DB.DTO;
-
-
+import SubGame.Story1;
 import SubGame.Story3;
 
 import Util.Util;
 
-
 public class Ex01 {
+	public static void main(String[] args) {
 
+	
+	}
 }
-  
-                                                                                        

648c29b6ea2f17aeabc7530c1ba594befee8cc83
smhrd <smhrd@DESKTOP-H83O5V9>
2024-04-18 11:40:21 +0900

    commit

--- GitTest01/src/PlayStation/Ex01.java
+++ GitTest01/src/PlayStation/Ex01.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 
 import DB.DAO;
 import DB.DTO;
-
-
+import MainGame.SentenceGame;
 import SubGame.Story3;
 
 import Util.Util;
 
 
 public class Ex01 {
+	public static void main(String[] args) {
 
+	}
 }
   
                                                                                         

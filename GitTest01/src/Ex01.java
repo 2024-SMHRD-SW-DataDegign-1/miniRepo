a109d1874d953375f9375e5661abfc0160c53dde
smhrd <smhrd@DESKTOP-H83O5V9>
2024-04-18 09:34:46 +0900

    commit

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -18,24 +18,6 @@ import Util.Util;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		
-		
-				
-			
-		Story2 str = new Story2();
-		str.play();
-		
-		
-		
-		//String str = "⣿⣿⣿⣿⠿⠿⠿⢿⡿⠿⠿⠿⢿⣿⣿⣿\n⣿⣿⣿⡇ ⣤⣤⣤⡇⠀⣤⣤⣤⣿⣿⣿\n⣿⣿⣿⣇ ⠉⠉⠉⡇⠀⠉⠉⠉⣿⣿⣿\n⣿⣿⣿⠿⠿⠿⠿⠀ ⠿ ⠿⠿⠿⣿⣿\n⣿⣿⣿⣤⣤⣤⠤⠤⠤⠤⢤⣤⣤⣿⣿⣿\n⣿⣿⣿⣿⠉⠀⣤⣤⣤⣤⡀⠈⢻⣿⣿⣿\n⣿⣿⣿⣿⣄⡀⠉⠙⠛⠉⠁⣠⣾⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿";
-		//Util.print(str);
-//		SentenceGame view = new SentenceGame();
-//		view.Play();
-		
-
-
-
-
 
 	}
 }
